ed8f8ef333cf7a5f353be6c7a626ad404b727d87
brianmanoti <Brianmanoti254@gmail.com>
2023-03-30 12:11:28 -0700

    1-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Daclares a square"""
+
+
+class Square:
+    """Represents a square"""
+
+    def __init__(self, size):
+        """Initialize a square
+            Args:
+                size: size of square
+        """
+        self.__size = size
