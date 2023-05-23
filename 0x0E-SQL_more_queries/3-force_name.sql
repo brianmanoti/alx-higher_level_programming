20b46f5c8cde9ae8c088cb4c03d84955ca499a97
Brianmanoti <brianmanoti254@gmail.com>
2023-05-23 06:42:52 +0000

    More about sql commands

---
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -0,0 +1,2 @@
+--creating a table and inserting filed types
+CREATE TABLE IF NOT EXIST force_name(id INT, name VARCHAR(256) NOT NULL);
---
+++ 0x0E-SQL_more_queries/4-never_empty.sql
@@ -0,0 +1,2 @@
+--create another Table
+CREATE TABLE IF NOT EXISTS id_not_null(id INT DEFAULT 1, name VARCHAR(256));
