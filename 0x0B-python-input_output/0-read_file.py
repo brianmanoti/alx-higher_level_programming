9c70dea15fdbea140bbb9d6da01aeaa8209f4383
Brianmanoti <brianmanoti254@gmail.com>
2023-05-27 12:35:42 +0000

    0-read_file.py

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -3,6 +3,6 @@
 
 def read_file(filename=""):
 """ Opens A file and redirects it as stdout"""
-	with open('filename', encoding='utf-8') as f:
-		for line in f:
-			print(line)
+	with open('filename', 'r', encoding='utf-8') as file1:
+		data = read(file1)
+		print(file1)
