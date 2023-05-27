1239060945c52173d998b0521ebf04c9529321c4
Brianmanoti <brianmanoti254@gmail.com>
2023-05-27 13:05:48 +0000

    0-read_file.py

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
+"""Defines a text file-reading function."""
+
+
 def read_file(filename=""):
-    with open(filename, "r", encoding="utf-8") as file:
-        content = file.read()
-        print(content)
+    """Print the contents of a UTF8 text file to stdout."""
+    with open(filename, "r", encoding="utf-8") as f:
+        print(f.read(), end="")
