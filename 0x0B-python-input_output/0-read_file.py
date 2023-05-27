301679046ca27af0de751743ccb3b6c725d4b3fe
Brianmanoti <brianmanoti254@gmail.com>
2023-05-27 12:29:58 +0000

    0-read_file.py

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -2,13 +2,7 @@
 
 
 def read_file(filename=""):
-    """
-    Reads a text file (UTF8) and prints it to stdout.
-
-    :param filename: The name of the file to read.
-    :type filename: str
-    """
-    with open(filename, mode='r', encoding='utf-8') as f:
-        for line in f:
-            print(line, end='')
-
+""" Opens A file and redirects it as stdout"""
+	with open('filename', encoding='utf-8') as f:
+		for line in f:
+			print(line)
