751186240944129cd27daa6cb6c9b6cdf8a40770
Brian Manoti <Brianmanoti254@gmail.com>
2023-06-13 14:27:47 +0000

    JAVASCRIPT ARGUMENTS

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,8 +1,2 @@
-!/usr/bin/node
-const args = process.argv.slice(2);
-
-if (args[0]) {
-  console.log(args[0]);
-} else {
-  console.log("No argument");
-}
+#!/usr/bin/node
+console.log(typeof process.argv[2] === 'undefined' ? 'No argument' : process.argv[2]);
