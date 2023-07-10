6ac041ae2806199924f8c8b2b65d652d7b1e82f6
brian manoti <brianmanoti@gmail.com>
2023-07-10 18:02:35 +0000

    Javascript

---
+++ 0x13-javascript_objects_scopes_closures/100-sorted.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+const dict = require('./101-data.js').dict;
+const output = {};
+
+for (const [key, value] of Object.entries(dict)) {
+  if (output[value] === undefined) {
+    output[value] = [];
+  }
+  output[value].push(key);
+}
+console.log(output);
