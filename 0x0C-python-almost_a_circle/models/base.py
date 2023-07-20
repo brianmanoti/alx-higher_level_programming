a87348c65bdc43591bac927ffd4859ae9d6f5761
brianmanoti <brianmanoti254@gmail.com>
2023-07-20 12:43:04 -0700

    First base class

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,13 +1,32 @@
 #!/usr/bin/python3
 
 class Base:
-    """Base class that takes Id as an argument
-    args: id - the identufication given to the first argument
     """
+    This is the Base class that represents a base object.
+
+    Attributes:
+        __nb_objects (int): A private class attribute to keep track of the number of instances created.
+        id (int): A public instance attribute representing the ID of the object.
+
+    Methods:
+        __init__(self, id=None): The constructor method to initialize a Base object.
+    """
+
     __nb_objects = 0
+
     def __init__(self, id=None):
+        """
+        Initialize a Base object.
+
+        Args:
+            id (int, optional): The ID for the object. If not provided, it will be auto-generated.
+
+        Returns:
+            None
+        """
         if id is not None:
             self.id = id
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
