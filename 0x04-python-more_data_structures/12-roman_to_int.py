e6ba26883cdecaa75b67dd80d268ab7305403c9e
brianmanoti <brianmanoti254@gmail.com>
2023-10-25 20:48:04 +0000

    Roman to str function

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,16 +1,19 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    rom_num = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
-    value = 0
-    if type(roman_string) is not str or roman_string is None:
+    """ converts a Roman numeral to an integer."""
+    romans = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
+    if (roman_string is None) or (type(roman_string) is not str):
         return 0
-    if len(roman_string) == 1:
-        return rom_num.get(roman_string)
-    for i in range(len(roman_string)):
-        if (i + 1) == len(roman_string):
-            value += rom_num.get(roman_string[i])
-        elif rom_num.get(roman_string[i]) >= rom_num.get(roman_string[i +1]):
-            value += rom_num.get(roman_string[i])
+
+    number = len(roman_string)
+    value_int = romans[roman_string[number-1]]
+    for i in range(number - 1, 0, -1):
+        current_value = romans[roman_string[i]]
+        previous_value = romans[roman_string[i-1]]
+
+        if previous_value >= current_value:
+            value_int += previous_value
         else:
-            value -= rom_num.get(roman_string[i])
-    return value
+            value_int -= previous_value
+
+    return value_int
