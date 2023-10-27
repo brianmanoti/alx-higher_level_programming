94de53d7dcc59e475184a0368fa8bfb5122b0774
brianmanoti <brianmanoti254@gmail.com>
2023-10-27 13:45:11 +0000

    getting started with ORM

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+""" Connect to database """
+
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                         passwd=argv[2], db=argv[3], name=argv[4], charset="utf8")
+
+    curs = db.cursor()
+    curs.execute('SELECT * FROM states WHERE name = argv[4]')
+    states = curs.fetchall()
+    for state in states:
+        print(state)
+
+    curs.close()
+    db.close()
