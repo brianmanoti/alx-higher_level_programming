c5bc34861c37acdf84a3263325fb89b9d80f1611
Brian manoti <108587053+brianmanoti@users.noreply.github.com>
2024-03-04 21:57:48 +0300

    Update 2-square.py

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,16 +1,18 @@
 #!/usr/bin/python3
-"""Declares a class called Square With Private Attributes"""
+"""Daclares a square"""
 
-class square:
-    """
-    A class called Square
 
-    arg: size- a private attribute for defining its size
-    """
+class Square:
+    """Represents a square"""
+
     def __init__(self, size=0):
-        """Initialises the size attribute"""
+        """Initialize a square
+            Args:
+                size: size of square
+        """
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
-        if size < 0:
+        elif size < 0:
             raise ValueError("size must be >= 0")
-        self.__size = size
+        else:
+            self.__size = size
